beead63eafece5527198d5946d5f38b09bfa95e7
yadsury20 <yadsury20@gmail.com>
2025-09-30 00:09:45 -0400

    Add files via upload

---
+++ my first Python program.py
@@ -0,0 +1,7 @@
+Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> c = "cats"
+>>> d = "dogs"
+>>> s = f"It is raining {c} and {d}."
+>>> print (s)
+It is raining cats and dogs.
